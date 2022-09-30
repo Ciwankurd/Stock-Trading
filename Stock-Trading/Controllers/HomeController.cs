720f9e505dfed6be00e7a64324d912e324ddb56c
solin <s351892@oslomet.no>
2022-09-30 13:50:12 +0200

    try

--- Stock-Trading/Controllers/HomeController.cs
+++ Stock-Trading/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 
 namespace Stock_Trading.Controllers
 {
+    //try
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
