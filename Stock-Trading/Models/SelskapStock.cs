df61a9a13da98f23855297774726700bc97f24f7
Nechrvan Abdulla Murad <s351927@oslomet.no>
2022-10-03 17:18:17 +0200

    helloffff

--- Stock-Trading/Models/SelskapStock.cs
+++ Stock-Trading/Models/SelskapStock.cs
@@ -6,6 +6,7 @@ namespace Stock_Trading.Models
     {
         // heee
         //fghhj
+        // Fadle
         public string Id { get; set; }
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string SelskapNavn { get; set; }
